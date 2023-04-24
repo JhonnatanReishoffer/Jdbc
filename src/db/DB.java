608f74f8dce0960d72a9aea9f81e698c66d11c76
Jhonnatan <jhonnatan.muniz@soulasalle.com>
2023-04-24 10:07:02 -0300

    First Query with Statement and ResultSet

--- src/db/DB.java
+++ src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -48,4 +50,26 @@ public class DB {
 		}
 	
 	}
+	
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch(SQLException e) {
+				throw new DbException("Error: " + e.getStackTrace());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch(SQLException e) {
+				throw new DbException("Error: " + e.getStackTrace());
+			}
+		}
+	}
+	
+	
 }
